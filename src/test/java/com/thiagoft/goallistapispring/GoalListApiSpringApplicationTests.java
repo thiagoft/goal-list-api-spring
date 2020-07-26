d570d512d2aa94a6e9c27e1cbf39fef312045719
Thiago Tahara <thiagoftahara@gmail.com>
2020-07-26 00:51:59 -0300

    Initial Setup

---
+++ src/test/java/com/thiagoft/goallistapispring/GoalListApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoft.goallistapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalListApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
